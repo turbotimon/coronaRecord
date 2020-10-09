c64597cee7d4adc88de43a672dbebaa93ba2f143
tturbo <null>
2020-10-09 13:23:53 +0200

    add class People

--- app/src/main/java/ch/ost/mge/testat/coronarecord/model/People.java
+++ app/src/main/java/ch/ost/mge/testat/coronarecord/model/People.java
@@ -1,4 +1,30 @@
 package ch.ost.mge.testat.coronarecord.model;
 
-public class People {
+import androidx.annotation.Nullable;
+
+import java.util.HashSet;
+
+public class People extends HashSet<Person> {
+
+    private HashSet<Person> people = new HashSet<>();
+
+    @Override
+    public int size() {
+        return super.size();
+    }
+
+    @Override
+    public boolean add(Person person) {
+        return super.add(person);
+    }
+
+    @Override
+    public boolean remove(@Nullable Object o) {
+        return super.remove(o);
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+    }
 }
