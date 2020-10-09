e911a25ce39b04817f95bd465ec576f9c7fb3435
tturbo <null>
2020-10-09 11:29:50 +0200

    rename LocationSelect+Activity

--- app/src/main/java/ch/ost/mge/testat/coronarecord/LocationSelectActivity.java
+++ app/src/main/java/ch/ost/mge/testat/coronarecord/LocationSelectActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LocationSelect extends AppCompatActivity {
+public class LocationSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ch/ost/mge/testat/coronarecord/MainActivity.java
+++ app/src/main/java/ch/ost/mge/testat/coronarecord/MainActivity.java
@@ -2,18 +2,24 @@ package ch.ost.mge.testat.coronarecord;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private
+    private MaterialButton btnEnterCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        btnEnterCode.setOnClickListener( v -> {
+            Intent intent = new Intent(this, LocationSelectActivity)
+        });
 
 
     }
